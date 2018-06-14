09c0a3245ea016c9d366f84fdf77077f9f9106cd
alulab <alulab@V20516.lab.inf.pucp.edu.pe>
2018-06-14 17:29:44 -0500

    Lab5 SO

--- 02 Material/dibujaFAT.py
+++ 02 Material/dibujaFAT.py
@@ -6,101 +6,133 @@
 import sys
 import pygame
 import os
- 
+
 # Definimos algunos colores
 NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
 VERDE = ( 0, 255, 0)
 ROJO = (255, 0, 0)
- 
+
 # Establecemos el LARGO y ALTO de cada celda de la retícula.
 LARGO  = 30
 ALTO = 30
- 
+
 # Establecemos el margen entre las celdas.
 MARGEN = 5
- 
+
 # Creamos un array bidimensional. Un array bidimensional
 # no es más que una lista de listas.
 grid = []
 for fila in range(20):
-    # Añadimos un array vacío que contendrá cada celda 
+    # Añadimos un array vacío que contendrá cada celda
     # en esta fila
     grid.append([])
     for columna in range(20):
         grid[fila].append((0,0)) # Añade una celda
- 
+
+
+fatType = "FAT32"
+dataStart = 0
+
+with open(sys.argv[1], "rb") as fimage:
+    #Revisamos el tipo de FAT de la imagen proporcionada
+    fimage.seek(54)
+    entry = fimage.read(8)
+    entry = str(entry)[2:7]
+
+    if entry == "FAT16":
+        fatType = "FAT16"
+
+    print(fatType)
+
+    if fatType == "FAT32":
+        fimage.seek(14)
+        numReserved = fimage.read(2)
+        numReserved = int.from_bytes(numReserved,byteorder='little')
+
+        fimage.seek(11)
+        bytesPerSect = fimage.read(2)
+        bytesPerSect = int.from_bytes(bytesPerSect, byteorder='little')
+
+        dataStart = bytesPerSect * numReserved
+        print(dataStart)
+    else:
+        fimage.seek(14)
+        numReserved = fimage.read(2)
+        numReserved = int.from_bytes(numReserved,byteorder='little')
+
+        fimage.seek(11)
+        bytesPerSect = fimage.read(2)
+        bytesPerSect = int.from_bytes(bytesPerSect, byteorder='little')
+        dataStart = bytesPerSect * numReserved
 
 with open(sys.argv[1],"rb") as fimage:
-    t1 = fimage.read(2)
-    t2 = fimage.read(2)
-    t1 = int.from_bytes(t1, byteorder='little')
-    t2 = int.from_bytes(t2, byteorder='little')
-    print(str(t1),str(t2))
-    fimage.seek(4096)
+    fimage.seek(dataStart)
+    print()
     for x in range(20):
         for y in range(20):
-            entry = fimage.read(2)
+            entry = fimage.read(2 if fatType == "FAT16" else 4)
             i = int.from_bytes(entry,byteorder='little')
+            #print(i)
             if x == 0 and y == 0:
                 grid[x][y] = (2,"R")
             elif x == 0 and y == 1:
                 grid[x][y] = (2,"R")
-            elif i == 65535:                
+            elif i == (0xFFFF if fatType == "FAT16" else 0x0FFFFFFF):
                 grid[x][y] = (1,"EOF")
-            elif i != 0: 
+            elif i != 0:
                 grid[x][y] = (1,str(i))
-            else:    
+            else:
                 grid[x][y] = (0,str(i))
-            
+
 # Inicializamos pygame
 pygame.init()
-  
+
 # Establecemos el LARGO y ALTO de la pantalla
 DIMENSION_VENTANA = [705, 705]
 pantalla = pygame.display.set_mode(DIMENSION_VENTANA)
- 
+
 # Establecemos el título de la pantalla.
 pygame.display.set_caption("FAT")
- 
+
 # Iteramos hasta que el usuario pulse el botón de salir.
 hecho = False
- 
+
 # Lo usamos para establecer cuán rápido de refresca la pantalla.
 reloj = pygame.time.Clock()
 
 fuente = pygame.font.Font(None, 21)
- 
+
 # -------- Bucle Principal del Programa-----------
 while not hecho:
-    for evento in pygame.event.get(): 
-        if evento.type == pygame.QUIT: 
-            hecho = True       
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            hecho = True
     # Establecemos el fondo de pantalla.
     pantalla.fill(NEGRO)
-        
+
     # Dibujamos la FAT
     for fila in range(20):
         for columna in range(20):
-            #print(fila,columna)            
+            #print(fila,columna)
             color = BLANCO
             if (grid[fila][columna])[0] == 1:
-                color = VERDE                
+                color = VERDE
             if (grid[fila][columna])[0] == 2:
-                color = ROJO                
+                color = ROJO
             X = (MARGEN+LARGO) * columna + MARGEN
             Y = (MARGEN+ALTO) * fila + MARGEN
             pygame.draw.rect(pantalla,color,[X,Y,LARGO,ALTO])
-            
-            t = (grid[fila][columna])[1]            
+
+            t = (grid[fila][columna])[1]
             texto1 = fuente.render(t, 0, (0, 0, 0))
-            pantalla.blit(texto1,(X,Y+5))  
-            
+            pantalla.blit(texto1,(X,Y+5))
+
     # Limitamos a 60 fotogramas por segundo.
     reloj.tick(60)
- 
+
     # Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
     pygame.display.flip()
-     
+
 fimage.close()
 pygame.quit()
